101cf0aaba18b76b7da57b10f467800d4745362b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-01-31 22:58:49 +0300

    Including track methods in application controller

--- lib/track.rb
+++ lib/track.rb
@@ -1,4 +1,5 @@
 require "track/engine"
+require 'track/track_methods'
 
 module Track
   # Your code goes here...
--- lib/track/engine.rb
+++ lib/track/engine.rb
@@ -1,4 +1,9 @@
 module Track
   class Engine < ::Rails::Engine
+    initializer  "track.load_track_methods" do
+      ActiveSupport.on_load(:action_controller) do
+        include Track::TrackMethods
+      end
+    end
   end
 end
